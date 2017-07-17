b8799bd37066b04d6e6cfc6d33f76fa4e8b216f2
shumin huang <ms.huang.shumin@gmail.com>
2017-07-18 01:00:23 +0800

    complete

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
-$(document).ready(function () {
+$(document).ready(function() {
   $quotes = $('.quotes')
   $quoteButton = $('.quoteButton')
   $tweetButton = $('a')
   $container = $('#container')
-  $twitDesc = $('meta[name="twitter:description"]')
-  $quoteButton.on('click', function () {
+  // $twitDesc = $('meta[name="twitter:description"]')
+  $quoteButton.on('click', function() {
     $.ajax({
       url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=<required>', // The URL to the API. You can get this in the API page of the API you intend to consume
       method: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
       data: {}, // Additional parameters here
       dataType: 'json',
-      success: function (data) {
+      success: function(data) {
         $quotes.html(data.quote)
         $container.html('')
-        $twitDesc.attr('content', data.quote)
-        // twttr.widgets.createShareButton(
-        //   '/',
-        //   document.getElementById('container'),
-        //   {
-        //     size: 'large',
-        //     text: data.quote
-        //   }
-        // )
-
+        twttr.widgets.createShareButton(
+          '/',
+          document.getElementById('container'), {
+            size: 'large',
+            text: data.quote
+          }
+        )
         // $tweetButton.data('text', data.quote)
         // .replace(/\s/g, '%20')
+        // $twitDesc.attr('content', data.quote)
       },
       // error: function(err) {
       //   alert(err);
       // },
-      beforeSend: function (xhr) {
+      beforeSend: function(xhr) {
         xhr.setRequestHeader('X-Mashape-Authorization', 'Gy5qvGErt9mshInarkyuo6B7ghA2p1fd5P1jsnLTfwF5pgUCMj') // Enter here your Mashape key
       }
     })
